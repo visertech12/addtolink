8602d931649da3590596dafaed241a93a4720079
visertech12 <i.am.visertech@gmail.com>
2025-09-08 19:42:24 +0500

    Update route.ts

--- app/api\auth\[...nextauth]/route.ts
+++ app/api\auth\[...nextauth]/route.ts
@@ -1,1 +1,27 @@
 
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) token.accessToken = account.access_token
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+  },
+}
+
+const handler = NextAuth(authOptions)
+
+// In App Router, you need to export GET and POST
+export { handler as GET, handler as POST }
